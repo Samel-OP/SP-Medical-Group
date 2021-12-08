82717346061c66c13686ea93cef4abbbe10e9203
Samel <samueldeoliveirapereira1@gmail.com>
2021-12-08 11:21:21 -0300

    Fazendo login

---
+++ Mobile/spMedicalGroupMobile/src/screens/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View, 
+    Pressable,
+} from 'react-native'
+
+import AsyncStorage from "react-native";
+
+import api from "../services/api";
