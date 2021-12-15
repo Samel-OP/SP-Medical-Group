2fbc8b9e1d704499a738819d698bedae3a64fbca
Samel <samueldeoliveirapereira1@gmail.com>
2021-12-15 11:50:29 -0300

    Terminando estilo de descricao

--- Mobile/spMedicalGroupMobile/src/screens/descricao.js
+++ Mobile/spMedicalGroupMobile/src/screens/descricao.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import {
     StyleSheet,
@@ -46,13 +46,122 @@ export default class Descricao extends Component {
 
     render() {
         return (
-            <View style={styles.containerLista}>
-                <Text>Coé</Text>
+            <View style={styles.containerConsulta}>
+                <View style={styles.boxHeader}>
+                    <Text style={styles.textHeader}>SP Medical Group</Text>
+                </View>
+
+                <View style={styles.containerConteudo}>
+                    <View style={styles.boxConteudo}>
+                        <View style={styles.boxTitulo}>
+                            <Text style={styles.textTitulo}>Consulta</Text>
+                            <View style={styles.barraTitulo} />
+                        </View>
+
+                        <View>
+                            <Text style={styles.textConsulta}>N° Consulta:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.textConsulta}>Nome Paciente:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.textConsulta}>Nome Médico:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.textConsulta}>Data da Consulta:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.textConsulta}>Situação:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={styles.textConsulta}>Descrição:</Text>
+                            <View style={styles.boxConsulta}>
+                                <View style={styles.boxDadosConusulta}>
+                                    <Text></Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-
+    boxHeader: {
+        width: 450,
+        height: 40,
+        backgroundColor: '#00A1F7',
+        justifyContent: 'center',
+        paddingLeft: 20
+    },
+    textHeader: {
+        fontFamily: 'OpenSans-Regular',
+        color: 'white',
+        fontSize: 18
+    },
+    containerConteudo: {
+        alignItems: 'center'
+    },
+    boxConteudo: {
+        width: 350,
+        height: 575,
+        backgroundColor: '#BDEBFE',
+        borderRadius: 10,
+        marginTop: 35
+    },
+    boxTitulo: {
+        alignItems: 'center',
+        paddingTop: 20
+    },
+    textTitulo: {
+        fontFamily: 'OpenSans-Bold',
+        color: 'black',
+        fontSize: 24
+    },
+    barraTitulo: {
+        width: 110,
+        height: 1,
+        backgroundColor: 'black'
+    },
+    boxConsulta: {
+        alignItems: 'center'
+    },
+    textConsulta: {
+        fontFamily: 'OpenSans-Regular',
+        color: 'black',
+        fontSize: 14
+    },
+    boxDadosConusulta: {
+        width: 300,
+        height: 40,
+        backgroundColor: 'white',
+        borderRadius: 5
+    },
 })
